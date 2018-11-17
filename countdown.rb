8497768142c7072ad51e30e4f7471f04a0ec736b
Jonathan Biro <jonnybrx@gmail.com>
2018-11-17 03:32:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -11,3 +11,16 @@ def countdown(number)
   end
   return 'HAPPY NEW YEAR!'
 end
+
+def countdown_with_sleep(secs)
+  sleep secs
+end
+
+def countdown(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    countdown_with_sleep 1
+  end
+  "HAPPY NEW YEAR!"
+end
